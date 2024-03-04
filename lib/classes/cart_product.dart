99d0a46cd85e641bbd2097f57c6ac9b0414ab739
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2024-03-04 23:44:29 +0500

    refactor cart_product.dart

--- lib/classes/cart_product.dart
+++ lib/classes/cart_product.dart
@@ -4,20 +4,54 @@ class CartProduct {
   late Product product;
   late int quantity;
 
+  /// Constructor for [CartProduct]
+  ///
+  /// Parameters:
+  ///   - product: [Product]
+  ///
+  /// Example:
+  /// ```dart
+  /// // Usage example of the function.
+  /// CartProduct obj = CartProduct(product);
+  /// ```
   CartProduct({required this.product}) {
     quantity = 1;
   }
 
+  /// Increases the quantity of a particular [CartProduct]
+  ///
+  /// Example:
+  /// ```dart
+  /// // Usage example of the function.
+  /// cartProduct.increaseQuantity();
+  /// ```
   void increaseQuantity() {
     quantity++;
   }
 
+  /// Decreases the quantity of a particular [CartProduct]
+  ///
+  /// Example:
+  /// ```dart
+  /// // Usage example of the function.
+  /// cartProduct.decreaseQuantity();
+  /// ```
   void decreaseQuantity() {
     if (quantity > 0) {
       quantity--;
     }
   }
 
+  /// Returns total price of a price according to its quantity [price * quantity]
+  ///
+  /// Returns:
+  ///   [int]
+  ///
+  /// Example:
+  /// ```dart
+  /// // Usage example of the function.
+  /// cartProduct.getTotalPrice();
+  /// ```
   int getTotalPrice() {
     return product.price * quantity;
   }
